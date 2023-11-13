576402dde25bc1529aac0ee8c7ff5e590faec794
hong <kevin0928@naver.com>
2023-11-13 21:41:09 +0900

    init

---
+++ src/main/java/oneulmwohaji/OneulMwohajiApplication.java
@@ -0,0 +1,13 @@
+package oneulmwohaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulMwohajiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulMwohajiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oneulmwohaji/OneulMwohajiApplicationTests.java
@@ -0,0 +1,13 @@
+package oneulmwohaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneulMwohajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
