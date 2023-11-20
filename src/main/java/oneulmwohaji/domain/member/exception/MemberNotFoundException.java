14dcac81b7f889a4bcc2603743f88b49b2edffc3
hong <kevin0928@naver.com>
2023-11-21 00:42:18 +0900

    feat : member가 존재하지 않는 경우 에러 처리

---
+++ src/main/java/oneulmwohaji/domain/member/exception/MemberNotFoundException.java
@@ -0,0 +1,11 @@
+package oneulmwohaji.domain.member.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+    public MemberNotFoundException(final String message) {
+        super(message);
+    }
+
+    public MemberNotFoundException() {
+        this("member를 찾을 수 없습니다.");
+    }
+}
