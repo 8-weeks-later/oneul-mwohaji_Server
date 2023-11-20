03f847c6904b151d6dcfae089317a146d2534020
hong <kevin0928@naver.com>
2023-11-21 00:24:12 +0900

    feat : ControllerAdvice 클래스 구현
    - 각 에러코드별로 에러들을 분류함

---
+++ src/main/java/oneulmwohaji/global/common/error/ControllerAdvice.java
@@ -0,0 +1,31 @@
+package oneulmwohaji.global.common.error;
+
+import oneulmwohaji.domain.member.exception.MemberNotFoundException;
+import oneulmwohaji.global.auth.jwt.exception.TokenException;
+import oneulmwohaji.global.auth.jwt.exception.TokenExpiredException;
+import oneulmwohaji.global.auth.jwt.exception.TokenUnsupportedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(final RuntimeException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler({
+            TokenExpiredException.class,
+            TokenUnsupportedException.class,
+            TokenException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequest(final RuntimeException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+}
