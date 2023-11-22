c1811d03cdd9e5c09ef585c72395c9e1e0efe926
hong <kevin0928@naver.com>
2023-11-22 15:43:44 +0900

    refactor : 사용하지 않는 import문 삭제

--- src/main/java/oneulmwohaji/domain/member/entity/Member.java
+++ src/main/java/oneulmwohaji/domain/member/entity/Member.java
@@ -1,22 +1,18 @@
 package oneulmwohaji.domain.member.entity;
 
 import java.util.Collection;
-import java.util.Map;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import oneulmwohaji.global.auth.oauth.entity.OAuthProvider;
 import oneulmwohaji.global.common.entity.BaseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Entity
 @Getter
