361f4e4a62ee7cc2bb0f9e11a8187e740192d808
hong <kevin0928@naver.com>
2023-11-22 14:04:09 +0900

    fix : OAuthCustomer에서 OAuthUser로 변경
    - user의 정보를 가져올 때 OAuth2User 객체를 통해 가져오도록 수정

--- src/main/java/oneulmwohaji/global/auth/oauth/service/OAuth2MemberSuccessHandler.java
+++ src/main/java/oneulmwohaji/global/auth/oauth/service/OAuth2MemberSuccessHandler.java
@@ -2,15 +2,13 @@ package oneulmwohaji.global.auth.oauth.service;
 
 import java.io.IOException;
 import java.net.URI;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oneulmwohaji.global.auth.jwt.service.JwtProvider;
-import oneulmwohaji.global.auth.oauth.entity.OAuth2CustomUser;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -23,21 +21,23 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-        OAuth2CustomUser oAuth2User = (OAuth2CustomUser) authentication.getPrincipal();
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String oAuthId = getOAuthIdFromOAuth2User(oAuth2User);
+        redirect(request, response, oAuthId);
+    }
 
-        redirect(request, response, authentication);  // Access Token과 Refresh Token을 Frontend에 전달하기 위해 Redirect
+    private String getOAuthIdFromOAuth2User(OAuth2User oAuth2User) {
+        return oAuth2User.getAttributes().get("id").toString();
     }
 
     private void redirect(HttpServletRequest request, HttpServletResponse response,
-                          Authentication authentication) throws IOException {
-        String accessToken = jwtProvider.createAccessToken(authentication);  // Access Token 생성
-        String refreshToken = jwtProvider.createRefreshToken(authentication);     // Refresh Token 생성
+                          String oAuthId) throws IOException {
+        String accessToken = jwtProvider.createAccessToken(oAuthId);  // Access Token 생성
+        String refreshToken = jwtProvider.createRefreshToken(oAuthId);   // Refresh Token 생성
 
-        String uri = createURI(accessToken, refreshToken).toString();   // Access Token과 Refresh Token을 포함한 URL을 생성
-        getRedirectStrategy().sendRedirect(request, response,
-                uri);   // sendRedirect() 메서드를 이용해 Frontend 애플리케이션 쪽으로 리다이렉트
+        String uri = createURI(accessToken, refreshToken).toString(); // Access Token과 Refresh Token을 포함한 URL을 생성
+        getRedirectStrategy().sendRedirect(request, response, uri);
     }
 
     // Redirect URI 생성. JWT를 쿼리 파라미터로 담아 전달한다.
@@ -49,7 +49,7 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
         return UriComponentsBuilder
                 .newInstance()
                 .scheme("https")
-                .host("munny")
+                .host("example")
                 .path("/oauth")
                 .queryParams(queryParams)
                 .build()
