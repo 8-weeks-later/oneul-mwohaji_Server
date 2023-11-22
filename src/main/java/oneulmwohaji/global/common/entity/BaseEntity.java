f44d63610126b65691816a1b00c5eebb64327a06
hong <kevin0928@naver.com>
2023-11-22 14:37:07 +0900

    fix : EntityListnerㄴ 어노테이션 수정
    - EntityListner.class에서 AuditingEntityListner로 수정

--- src/main/java/oneulmwohaji/global/common/entity/BaseEntity.java
+++ src/main/java/oneulmwohaji/global/common/entity/BaseEntity.java
@@ -7,9 +7,10 @@ import javax.persistence.MappedSuperclass;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity {
 
