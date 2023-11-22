d82509796f0c136e3571367fc98d8497c035b01d
hong <kevin0928@naver.com>
2023-11-22 13:58:58 +0900

    feat : 생성시간, 수정시간 저장을 위한 BaseEntity 생성

---
+++ src/main/java/oneulmwohaji/global/common/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package oneulmwohaji.global.common.entity;
+
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Data
+@EntityListeners(EntityListeners.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at", updatable = false)
+    @LastModifiedDate
+    private Instant updatedAt;
+}
