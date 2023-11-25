9d3bdd2eea9de7cd4ec80df7afb7a7d76f5f9df1
hong <kevin0928@naver.com>
2023-11-25 15:02:55 +0900

    feat : oauth 로그인이 실패한 경우 호출하는 uri를 처리하는 controller 생성
    - errorCode는 400으로 반환함

---
+++ src/main/java/oneulmwohaji/domain/member/controller/MemberController.java
@@ -0,0 +1,24 @@
+package oneulmwohaji.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import oneulmwohaji.domain.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/auth/login")
+    public ResponseEntity<?> authLoginFailure(
+            @RequestParam("error") String error,
+            @RequestParam("errorMessage") String errorMessage
+    ) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorMessage);
+    }
+}
