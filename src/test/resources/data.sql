02aa2aece090f1d95d33d5e90c4b23159b3e4ec7
hong <kevin0928@naver.com>
2023-12-30 15:55:43 +0900

    git remove cache

--- src/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
-VALUES ('admin@example.com', 'admin', 'admin123', 'NAVER', false, 'ROLE_ADMIN');
-
-INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
-VALUES ('user@naver.com', 'admin', '112233', 'NAVER', true, 'ROLE_USER');
-
-
-INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
-VALUES ('kevin0928@naver.com', 'admin', '123123123', 'NAVER', false, 'ROLE_ADMIN');
