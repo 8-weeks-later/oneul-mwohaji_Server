3b669b1376ea976f820659eeeafa4a5d7cdcae5e
hong <kevin0928@naver.com>
2023-12-31 01:15:04 +0900

    test : postService 테스트 코드 작성
    - 거리별 테스트 코드 작성
    - 5개의 레스토랑을 만든 뒤, 테스트 구현

---
+++ src/test/java/oneulmwohaji/domain/post/service/PostServiceTest.java
@@ -0,0 +1,78 @@
+package oneulmwohaji.domain.post.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import oneulmwohaji.domain.post.dto.request.UserGeometryInfoDto;
+import oneulmwohaji.domain.post.dto.response.PostResponseDto;
+import oneulmwohaji.domain.post.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceTest {
+    @Autowired
+    private PostRepository postRepository;
+    private PostService postService;
+    private UserGeometryInfoDto userGeometryInfoDto;
+
+    @BeforeEach
+    void setUp() {
+        postService = new PostService(postRepository);
+    }
+
+    @DisplayName("사용자의 위치를 기준으로 레스토랑 조회 테스트 : 조회 테스트")
+    @Test
+    public void getPostsByUserGeometry() throws Exception {
+        //given
+        userGeometryInfoDto = new UserGeometryInfoDto(37.5665, 26.9780, 10); // 10이면 111 * 10 -> 1.1km이다.
+        //when
+        List<PostResponseDto> postResponseDtos = postService.getPostsByUserGeometry(userGeometryInfoDto);
+        //then
+        assertEquals(1, postResponseDtos.size());
+    }
+
+    @DisplayName("사용자의 위치를 기준으로 레스토랑 조회 테스트 : range 안에 레스토랑이 없는 경우 테스트")
+    @Test
+    public void getPostsByUserGeometryIfThereIsNoRestraurantInRange()throws Exception {
+        //given
+        userGeometryInfoDto = new UserGeometryInfoDto(47.5665, 26.9780, 10); // 10이면 111 * 10 -> 1.1km이다.
+        //when
+        List<PostResponseDto> postResponseDtos = postService.getPostsByUserGeometry(userGeometryInfoDto);
+        //then
+        assertEquals(0, postResponseDtos.size());
+    }
+    @DisplayName("사용자의 위치를 기준으로 레스토랑 조회 테스트 : range 안에 1개의 레스토랑이 있는 경우 테스트")
+    @Test
+    public void getPostsByUserGeometryIfOneRestraurantInRange() throws Exception {
+        //given
+        userGeometryInfoDto = new UserGeometryInfoDto(37.5665, 26.9780, 30); // 10이면 111 * 10 -> 1.1km이다.
+        //when
+        List<PostResponseDto> postResponseDtos = postService.getPostsByUserGeometry(userGeometryInfoDto);
+        //then
+        assertEquals(3, postResponseDtos.size());
+    }
+
+    @DisplayName("사용자의 위치를 기준으로 레스토랑 조회 테스트 : range 안에 여러 레스토랑이 있는 경우 테스트")
+    @Test
+    public void getPostsByUserGeometryIfMultipleRestaurantsInRange() throws Exception {
+        //given
+        userGeometryInfoDto = new UserGeometryInfoDto(37.5785, 26.9780, 200000); // 해당 좌표 주변에 여러 개의 레스토랑이 있다는 가정
+        //when
+        List<PostResponseDto> postResponseDtos = postService.getPostsByUserGeometry(userGeometryInfoDto);
+        //then
+        assertTrue(postResponseDtos.size() > 1);
+    }
+
+    @DisplayName("사용자의 위치를 기준으로 레스토랑 조회 테스트 : 잘못된 좌표로 인한 예외 테스트")
+    @Test
+    public void getPostsByUserGeometryIfInvalidCoordinates() {
+        //given
+        userGeometryInfoDto = new UserGeometryInfoDto(200.0, 200.0, 100); // 유효하지 않은 좌표를 전달하는 가정
+        //when & then
+        assertThrows(Exception.class, () -> postService.getPostsByUserGeometry(userGeometryInfoDto));
+    }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,26 @@
+--- member 데이터
+INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
+VALUES ('admin@example.com', 'admin', 'admin123', 'NAVER', false, 'ROLE_ADMIN');
+
+INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
+VALUES ('user@naver.com', 'admin', '112233', 'NAVER', true, 'ROLE_USER');
+
+
+INSERT INTO member (email, username, oauth_id, o_auth_provider, is_ban, account_type)
+VALUES ('kevin0928@naver.com', 'admin', '123123123', 'NAVER', false, 'ROLE_ADMIN');
+
+--- Restaurant 데이터
+INSERT INTO Post (point, restaurant_name, scrap_count)
+VALUES (ST_GeomFromText('POINT(26.9780 37.5665)', 4326), '한식', 0);
+
+INSERT INTO Post (point, restaurant_name, scrap_count)
+VALUES (ST_GeomFromText('POINT(26.9781 37.5666)', 4326), '양식', 0);
+
+INSERT INTO Post (point, restaurant_name, scrap_count)
+VALUES (ST_GeomFromText('POINT(26.9782 37.5667)', 4326), '중식', 0);
+
+INSERT INTO Post (point, restaurant_name, scrap_count)
+VALUES (ST_GeomFromText('POINT(26.9783 37.5668)', 4326), '일식', 0);
+
+INSERT INTO Post (point, restaurant_name, scrap_count)
+VALUES (ST_GeomFromText('POINT(26.9784 37.5669)', 4326), '그 외', 0);
